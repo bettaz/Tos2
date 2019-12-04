6b1ec7f9c405f5d2cac31bdb0392f0bfdbd3207e
Alessio Bettarello <bettaz.developer@gmail.com>
2019-12-04 17:30:51 +0100

    close #10

--- src/test/java/it/unipd/tos/business/OrderCalculatorTest.java
+++ src/test/java/it/unipd/tos/business/OrderCalculatorTest.java
@@ -40,4 +40,20 @@ public class OrderCalculatorTest {
         double total = calculator.getOrderPrice(voidOrder);
         assertEquals(0.0, total,0.0);
     }
+    /** Test simple bill
+     * with more than 10€, less than 5 sandwiches, less than 50€ of total order,
+     * less than 30 entries
+     */
+    @Test
+    public void SimpleOrderList_Test() throws TakeAwayBillException
+    {
+        List<MenuItem> simpleOrder = new ArrayList<MenuItem>();
+        OrderTotalBill calculator = new OrderTotalBill();
+        simpleOrder.add(new MenuItem("panino 1",5.0, MenuItem.itemType.Panino));
+        simpleOrder.add(new MenuItem("panino 2",6.0, MenuItem.itemType.Panino));
+        simpleOrder.add(new MenuItem("cola",3.5,MenuItem.itemType.Bevanda));
+        simpleOrder.add(new MenuItem("oliva ascolana", 3.0, MenuItem.itemType.Fritto));
+        double total = calculator.getOrderPrice(simpleOrder);
+        assertEquals(17.5, total,0.0);
+    }
 }
