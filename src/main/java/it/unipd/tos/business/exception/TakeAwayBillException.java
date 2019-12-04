94908976cc091c1f036d42b886108219668967b7
Alessio Bettarello <bettaz.developer@gmail.com>
2019-12-04 17:51:00 +0100

    close #16

--- src/main/java/it/unipd/tos/business/exception/TakeAwayBillException.java
+++ src/main/java/it/unipd/tos/business/exception/TakeAwayBillException.java
@@ -3,5 +3,14 @@
 ////////////////////////////////////////////////////////////////////
 package it.unipd.tos.business.exception;
 
-public class TakeAwayBillException {
+public class TakeAwayBillException extends Throwable {
+    private String message;
+    public TakeAwayBillException(String msg){
+        message=msg;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
 }
