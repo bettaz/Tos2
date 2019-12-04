01dd20702f6174b2995a08eb47765ba4699bd805
Alessio Bettarello <bettaz.developer@gmail.com>
2019-12-04 17:52:02 +0100

    close #15

--- src/main/java/it/unipd/tos/model/MenuItem.java
+++ src/main/java/it/unipd/tos/model/MenuItem.java
@@ -4,4 +4,25 @@
 package it.unipd.tos.model;
 
 public class MenuItem {
+    public enum itemType {Panino, Fritto, Bevanda};
+    private String name;
+    private double price;
+    private itemType itemType;
+    public MenuItem(String name, double price, itemType itemType){
+
+        this.name = name;
+        this.price = price;
+        this.itemType = itemType;
+    }
+    public String GetName() {
+        return name;
+    }
+
+    public double GetPrice() {
+        return price;
+    }
+
+    public itemType GetItemType() {
+        return itemType;
+    }
 }
